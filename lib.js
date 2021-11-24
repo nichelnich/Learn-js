6432fe493053261f65ae8f3292fe0da2b8b67e84
nichel <nichelnich@gmail.com>
2021-11-24 14:52:29 +0800

    Create lib.js

---
+++ lib.js
@@ -0,0 +1,19 @@
+ function dataToBool (data) {
+    if (data == null) return false;
+    if ((data + '') === "") return false;
+    if ((data + '') === '0') return false;
+    if ((data + '').toLowerCase() === 'false') return false;
+    if ((data + '').toLowerCase() === 'disable') return false;
+    return true;
+};
+
+function isDataEmpty (data) {
+    if (data == null) return true;
+    if (data === "") return true;
+    return false;
+};
+
+// test string format
+// const str = 'hello world!';
+// const result = /^hello/.test(str); //is str == null, no excepion
+// console.log(result); // true
